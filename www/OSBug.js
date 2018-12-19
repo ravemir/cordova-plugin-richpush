0814610be3ce30e95f2aa49a85df1cf4e8be4af0
Carlos Filipe Simões <ravemir@users.noreply.github.com>
2018-12-19 18:02:24 +0000

    Added JS module.

--- www/OSBug.js
+++ www/OSBug.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
                    
 exports.throwError = function(message, success, fail) {
-    exec(success, fail, "OSBugPlugin", "throwError", [message]);
+    exec(success, fail, "OSRichPushPlugin", "throwError", [message]);
 }
