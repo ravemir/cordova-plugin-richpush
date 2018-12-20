5777eb614b2abddb6ef4ce4c744cf1f4d5dba966
Carlos Filipe Simões <ravemir@users.noreply.github.com>
2018-12-20 20:49:47 +0000

    Fixed methods not running.

--- www/OSRichPush.js
+++ www/OSRichPush.js
@@ -2,5 +2,5 @@ var exec = require('cordova/exec');
                    
 exports.richifyPushNotification = function(message, success, fail) {
     // Invoke native methods
-    exec(success, fail, "OSRichPushPlugin", "richifyPushNotification", [message]);
+    exec(success, fail, "OSRichPush", "richifyPushNotification", [message]);
 }
